ab32809d4f8174df6770d1728697174700d465b9
Tom Bushmitz <tombushmitz@gmail.com>
2018-06-09 06:59:04 -0700

    Add host in CIDR lookup

---
+++ hosts.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net"
+)
+
+func GetHosts(cidr string) ([]net.IP, error) {
+	ip, ipnet, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return nil, err
+	}
+
+	var ips []net.IP
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+		ips = append(ips, ip)
+	}
+	// remove network address and broadcast address
+	return ips[1 : len(ips)-1], nil
+}
+
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
